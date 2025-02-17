24f918e3c3e477be1f1c32a5c3ddb1c060f4461f
Rita <rita.barragan@usp.ceu.es>
2025-02-17 21:08:44 +0100

    ejer 8 y su correccion en clase

--- src/App.java
+++ src/App.java
@@ -3,25 +3,7 @@ import java.util.List;
 
 public class App {
     // EJERCICIOS RECURSIVIDAD
-    // EJERCICIO 8: Suma de los elementos pares de una lista:
-/*
-    public static int sumaListaPares(ArrayList lista){
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        //int lista[]=0;
-
-        for(int i=0; i>=lista.size; i++){
-            if (n % 2 == 0){
-                return listaSuma.add;
-            }
-            if(n % 2 == 1){
-                System.out.println("el numero es impar");
-            }
-        
-        //devuelve la suma de esa lista
-        return listaSuma.sum;
-        }
-    }
-    
+    // EJERCICIO 8: Suma de los elementos pares de una lista:  
 
     
     public static int sumaListaPares(int[] arr, int index) {
@@ -40,8 +22,7 @@ public class App {
         //Si es así, se devuelve el elemento actual más la suma de los elementos pares restantes.
         //De lo contrario, se devuelve la suma de los elementos pares restantes.
 
-*/
-
+    /* 
     //CORRECCION EJER8 EN CLASE:
     sumaPares(ArrayList<Integer> lista, int indice){
         if (indice >= lista.size()){
@@ -52,7 +33,7 @@ public class App {
             }
         }
     }
-
+*/
 
     //EJERCICIO 9: dada una lista, obten otra lista con los numeros pares de la lista original
     public static List<Integer> ObtenerListaPar(ArrayList lista1, ArrayList lista2, posicion) {
@@ -62,6 +43,7 @@ public class App {
             return ObtenerListaPar(lista2);
         }
     }
+    
 
     public static void main(String args[]) {
         System.out.println("Hola mundo");
