9f6aafebb3fabbc0006a1460b6f020dc34845962
Rita <rita.barragan@usp.ceu.es>
2025-02-17 21:33:27 +0100

    añadido enunciados Recursivo

--- src/Recursivo.java
+++ src/Recursivo.java
@@ -3,7 +3,24 @@ import java.util.List;
 
 public class Recursivo{
     // EJERCICIOS RECURSIVIDAD
-    // EJERCICIO 8: Suma de los elementos pares de una lista:  
+
+    //EJERCICIO 1: La suma 0 + 1 + 2 + ... + n.
+
+    //EJERCICIO 2: El factorial de un número.
+
+    //EJERCICIO 3: La potencia n-ésima de un número natural.
+
+    //EJERCICIO 4: La suma de los elementos de una lista de números.
+
+    //EJERCICIO 5: La media aritmética de una lista de números.
+
+    //EJERCICIO 6: La desviación típica de una lista de números.
+
+    //EJERCICIO 7: La suma de los primeros números pares hasta n asumiendo n ≥ 2. 
+    //Por ejemplo, sumaP ares(9) = 8 + 6 + 4 + 2 = 20.
+
+    //EJERCICIO 8: La suma de los elementos pares de una lista de enteros. 
+    //Por ejemplo, sumaP ares([1, 2, 3, 4]) = 6.  
 
     
     public static int sumaListaPares(int[] arr, int index) {
