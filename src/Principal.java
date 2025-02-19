1d55c8ec365fbf4b0c8985fb267cd9fe7b4c4ad0
Rita <rita.barragan@usp.ceu.es>
2025-02-19 23:31:28 +0100

    añadidas pruebas Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -1,11 +1,29 @@
 public class Principal {
     public static void main(String[] args) {
-        
-        // Pruebas de los métodos en Iterativo.java
-        System.out.println("Suma iterativa de 5: " + Iterativo.suma(5));
-        System.out.println("Factorial iterativo de 5: " + Iterativo.factorial(5));
 
-        // Pruebas de los métodos en Recursivo.java
+        //pruebas de los métodos en Iterativo.java
+        //prueba ejer1:
+        System.out.println("Suma iterativa de 5: " + Iterativo.suma(5));
+        //prueba ejer2:
+        System.out.println("Factorial iterativo de 5: " + Iterativo.factorial(5));
+        //prueba ejer3:
+        //prueba ejer4:
+        //prueba ejer5:
+        //prueba ejer6:
+        //prueba ejer7:
+        //prueba ejer8:
+        //prueba ejer9:
+
+        //pruebas de los métodos en Recursivo.java
+        //prueba eje1:
+        //prueba ejer2:
+        //prueba ejer3:
+        //prueba ejer4:
+        //prueba ejer5:
+        //prueba ejer6:
+        //prueba ejer7:
+        //prueba ejer8:
+        //prueba ejer9:
         int[] lista = {1, 2, 3, 4, 5, 6};
         System.out.println("Suma de pares recursiva en lista: " + Recursivo.sumaListaPares(lista, 0));
     }
